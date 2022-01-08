007ede59ef7e58bff7a95d507ac4d999d2b15e37
Sean Kan <59797882+sclkan@users.noreply.github.com>
2022-01-08 15:17:40 +0900

    Created and tested GroceriesFileReader that reads input file

---
+++ src/subsetsum/GroceriesFileReader.java
@@ -0,0 +1,41 @@
+package subsetsum;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * Class to read input file that contains prices of different grocery items
+ * @author Sean Kan
+ *
+ */
+
+public class GroceriesFileReader
+{
+    /**
+     *
+     * @param filePath          Location of the input file
+     * @return an arrayList of price (Double)
+     */
+    public ArrayList<Double> readFile(String filePath)
+    {
+        ArrayList<Double> price = new ArrayList<Double>();
+        try
+        {
+            Scanner reader = new Scanner(new File(filePath));
+            while (reader.hasNextLine())
+            {
+                String input = reader.nextLine();
+                String[] temp = input.split(",");
+                price.add(Double.parseDouble(temp[1]));
+            }
+            reader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return price;
+    }
+}
